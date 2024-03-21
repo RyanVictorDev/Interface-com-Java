a00c8c36fdcb0ff0b34185e3e3f2133950334185
RyanVictorDev <vryan8294@gmail.com>
2024-03-21 11:27:50 -0300

    Primeiro commit

--- src/MainInterface.java
+++ src/MainInterface.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class MainInterface {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainInterface extends JFrame{
+    private javax.swing.JLabel LabelTitle1;
+    private JTextField digiteAlgoAquiTextField;
+    private JButton cliqueEmMimButton;
+    private JPanel section1, section2;
+    private JButton criarNovaTelaButton;
+
+    public MainInterface(){
+        setContentPane(section1);
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        cliqueEmMimButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String texto = digiteAlgoAquiTextField.getText();
+                LabelTitle1.setText(texto);
+
+            }
+        });
+        criarNovaTelaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Section2();
+            }
+        });
+    }
+    public static void main(String[] args) {
+        new MainInterface();
+    }
 }
--- src/Section2.java
+++ src/Section2.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Section2 {
+import javax.swing.*;
+public class Section2 extends JFrame{
+    private JPanel section2;
+    private JLabel labelTitle2;
+
+    public Section2(){
+        setContentPane(section2);
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+
 }
